fd915069f5c2cac1c4703122eeb4a5b3d5c123b6
kaeri <a77088554@gmail.com>
2026-03-05 16:20:48 +0800

    feat: 增加使用者互動時元素顯示動畫

--- src/features/Attraction/views/AttractionView.tsx
+++ src/features/Attraction/views/AttractionView.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import Favorites from "../components/Favorites";
 import Main from "../components/Main";
 import Search from "../components/Search";
@@ -5,9 +6,30 @@ import useFavorites from "../hooks/useFavorites";
 
 function AttractionView(){
     const favoritesHook = useFavorites()
+    const targetRef = useRef(null)
+    const [isVisible, setIsVisible] = useState(false)    
     
+    useEffect(()=>{
+        const options = {
+            threshold: 0.5,
+        }
+        const observer = new IntersectionObserver((entries)=>{
+            const [entry] = entries
+            if(entry.isIntersecting){
+                setIsVisible(entry.isIntersecting)
+                observer.disconnect()
+            }
+        },options)
+
+        if(targetRef.current) observer.observe(targetRef.current)
+        
+        return ()=>{
+            observer.disconnect()
+        } 
+    },[targetRef])
+
     return(
-        <div className="w-full py-8 overflow-x-hidden main-bg-text-color flex-col-center">
+        <div className={`w-full py-8 overflow-x-hidden main-bg-text-color flex-col-center transition duration-600 ${!isVisible ? 'opacity-0': 'opacity-100'}`} ref={targetRef}>
             <Main {...favoritesHook}/> 
             <div className="flex-col-center gap-5">
                 <Search {...favoritesHook}/>
--- src/features/Plan/views/PlanView.tsx
+++ src/features/Plan/views/PlanView.tsx
@@ -1,10 +1,33 @@
+import { useEffect, useRef, useState } from "react"
 import Plan from "../components/Plan"
 
 function PlanView(){
+    const targetRef = useRef(null)
+    const [isVisible, setIsVisible] = useState(false)    
+        
+    useEffect(()=>{
+        const options = {
+            threshold: 0.5,
+        }
+        const observer = new IntersectionObserver((entries)=>{
+            const [entry] = entries
+            if(entry.isIntersecting){
+                setIsVisible(entry.isIntersecting)
+                observer.disconnect()
+            }
+        },options)
+
+        if(targetRef.current) observer.observe(targetRef.current)
+        
+        return ()=>{
+            observer.disconnect()
+        } 
+    },[targetRef])
+
     return(
-        <>
+        <div className={`flex-col-center transition duration-600 ${!isVisible? 'opacity-0': 'opacity-100'}`} ref={targetRef}>
             <Plan/>
-        </>
+        </div>
     )
 }
 
